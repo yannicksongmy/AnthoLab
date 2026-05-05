fa0581b797c6809fe52d316f1e1fb4d0610f3017
Yannick Songmy <yannicksongmy@gmail.com>
2026-05-05 13:31:06 +0100

    Add Google verification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yannicksongmy.github.io/',
   base: '/AnthoLab',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
